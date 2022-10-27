0a92ccc2650fbc8c010d55e629efb1146e250d03
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-10-27 21:18:22 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+class Student < User
+    
+    def initialize
+        @knowledge = []
+     end
 
+     def learn(subject)
+        @knowledge << subject
+     end
+
+     def knowledge
+        @knowledge
+     end
+   
 end
